e7426a2eaba8cd819819ee0e767103d7aa30e325
mfaras94 <mfaras94@gmail.com>
2026-02-16 04:30:32 +0500

    signup controller changes

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -38,9 +38,12 @@ export const signup = async (req,res) => {
         })
 
         if(newUser){
-            generateToken(newUser._id,res)
+            // generateToken(newUser._id,res)
+            // await newUser.save()
 
-            await newUser.save()
+              const savedUser = await newUser.save()
+                generateToken(savedUser._id,res)
+
 
             return res.status(201).json({
                 _id:newUser._id,
