fa26e71ab346673070fea0fa4dd99a8fd1f89322
mfaras94 <mfaras94@gmail.com>
2026-02-17 08:15:47 +0500

    rateLimit-implemented

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,7 @@ import path from "path"
 
 import authRoutes from "./routes/auth.route.js"
 import messageRoutes from "./routes/message.route.js"
+import { apiLimiter, authLimiter } from "./middleware/rateLimit.middleware.js"
 import { connectDB } from "./lib/db.js"
 import { ENV } from "./lib/env.js"
 
@@ -17,8 +18,13 @@ const PORT = ENV.PORT || 3000
 app.use(express.json())
 app.use(cookieParser())
 
-app.use("/api/auth",authRoutes)
+// Current behavior:
+// "/api/auth/*" uses authLimiter only 
+app.use("/api/auth",authLimiter,authRoutes)
+// Other API routes like "/api/message/*" go through apiLimiter 
+app.use("/api",apiLimiter)
 app.use("/api/message", messageRoutes)
+
 
 if(ENV.NODE_ENV === "production"){
     app.use(express.static(path.join(__dirname,"../frontend/dist")))
