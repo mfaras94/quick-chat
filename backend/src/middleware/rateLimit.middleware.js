1839505a41293dfaae88f5b6ba1475bc7b374bfc
mfaras94 <mfaras94@gmail.com>
2026-02-17 08:22:21 +0500

    ratelimit fix

--- backend/src/middleware/rateLimit.middleware.js
+++ backend/src/middleware/rateLimit.middleware.js
@@ -1,14 +1,8 @@
 import  rateLimit from "express-rate-limit"
 
-export const apiLimiter = rateLimit({
+export const rateLimiter = rateLimit({
     windowMs: 15 * 60 * 1000,
-    max:300,
+    max:100,
     message: { message: "Too many requests, please try again later." },
-
-})
-export const authLimiter = rateLimit({
-    windowMs: 15 * 60 * 1000,
-    max:10,
-    message: { message: "Too many auth attempts, please try again later." },
 
 })
--- backend/src/server.js
+++ backend/src/server.js
@@ -4,7 +4,7 @@ import path from "path"
 
 import authRoutes from "./routes/auth.route.js"
 import messageRoutes from "./routes/message.route.js"
-import { apiLimiter, authLimiter } from "./middleware/rateLimit.middleware.js"
+import { rateLimiter } from "./middleware/rateLimit.middleware.js"
 import { connectDB } from "./lib/db.js"
 import { ENV } from "./lib/env.js"
 
@@ -18,11 +18,8 @@ const PORT = ENV.PORT || 3000
 app.use(express.json())
 app.use(cookieParser())
 
-// Current behavior:
-// "/api/auth/*" uses authLimiter only 
-app.use("/api/auth",authLimiter,authRoutes)
-// Other API routes like "/api/message/*" go through apiLimiter 
-app.use("/api",apiLimiter)
+app.use(rateLimiter)
+app.use("/api/auth",authRoutes)
 app.use("/api/message", messageRoutes)
 
 
