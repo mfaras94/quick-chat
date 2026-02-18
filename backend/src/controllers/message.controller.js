0036171ed2082a322c2e6d88be55d4a682b2ea8f
mfaras94 <mfaras94@gmail.com>
2026-02-18 05:55:44 +0500

    improvment

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -43,9 +43,16 @@ export const sendMessage = async (req, res) => {
     const senderId = req.user._id;
     const { id: receiverId } = req.params;
 
-    if (!senderId)
-      return res.status(400).json({ message: "You are not login" });
-
+    if (!text && !image) {
+      return res.status(400).json({ message: "Text or image is required." });
+    }
+    if (senderId.equals(receiverId)) {
+      return res.status(400).json({ message: "Cannot send messages to yourself." });
+    }
+    const receiverExists = await User.exists({ _id: receiverId });
+    if (!receiverExists) {
+      return res.status(404).json({ message: "Receiver not found." });
+    }
     let imageURL;
     if (image) {
       const uploadResponse = await cloudinary.uploader.upload(imageURL);
