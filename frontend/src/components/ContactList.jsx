882d4a2da51fa848efee73ae3f9abebe3ad38172
mfaras94 <mfaras94@gmail.com>
2026-02-19 07:49:04 +0500

    chatPage chats and contact API integrated

--- frontend/src/components/ContactList.jsx
+++ frontend/src/components/ContactList.jsx
@@ -1,16 +1,37 @@
+import { useEffect } from "react";
+import { useChatStore } from "../store/useChatStore";
+import UsersLoadingSkeleton from "../components/UsersLoadingSkeleton";
+
 const ContactList = () => {
+
+    const { allContacts, getAllContacts, isUsersLoading } = useChatStore();
+
+     useEffect(() => {
+    getAllContacts();
+  }, [getAllContacts]);
+
+    if (isUsersLoading) return <UsersLoadingSkeleton />;
+
   return (
     <>
-      <div className="bg-emerald-500/10 p-4 rounded-lg cursor-pointer hover:bg-emerald-500/20 transition-colors border border-zinc-700/40">
+    {allContacts.map(contact => (
+
+      <div
+        key={contact._id}
+      className="bg-emerald-500/10 p-4 rounded-lg cursor-pointer hover:bg-emerald-500/20 transition-colors border border-zinc-700/40">
         <div className="flex items-center gap-3">
           <div className={`avatar `}>
             <div className="size-12 rounded-full">
-              <img src={"/avatar.png"} />
+             <img
+                  src={contact.profilePic || "/avatar.png"}
+                  alt={contact.fullName}
+                />
             </div>
           </div>
-          <h4 className="text-zinc-200 font-medium">jon</h4>
+          <h4 className="text-zinc-200 font-medium">{contact.fullName}</h4>
         </div>
       </div>
+    ))}
     </>
   );
 };
