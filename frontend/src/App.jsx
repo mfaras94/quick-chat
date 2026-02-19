bd26bbd75e65471a5926a508153eecc169e8f8f9
mfaras94 <mfaras94@gmail.com>
2026-02-19 05:31:46 +0500

    theme change and logout api integrated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,11 @@
 import { Routes, Route, Navigate } from "react-router";
-import {Toaster} from "react-hot-toast"
 import ChatPage from "./pages/ChatPage";
 import LoginPage from "./pages/LoginPage";
 import SignUpPage from "./pages/SignUpPage";
 import PageLoader from "./components/PageLoader";
 import { useAuthStore } from "./store/useAuthStore";
 import { useEffect } from "react";
+import CustomToast from "./components/CustomToast";
 
 const App = () => {
   const {checkAuth, authUser , isCheckingAuth} = useAuthStore()
@@ -16,11 +16,11 @@ const App = () => {
 
   if(isCheckingAuth) return <PageLoader />
   return (
-    <div className="min-h-screen bg-slate-900 relative flex justify-center items-center p-4 overflow-hidden">
+    <div className="min-h-screen bg-zinc-900 relative flex justify-center items-center p-4 overflow-hidden">
      {/* DECORATORS - GRID BG & GLOW SHAPES */}
       <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px]" />
-      <div className="absolute top-0 -left-4 size-96 bg-pink-500 opacity-20 blur-[100px]" />
-      <div className="absolute bottom-0 -right-4 size-96 bg-cyan-500 opacity-20 blur-[100px]" />
+      <div className="absolute top-0 -left-4 size-96 bg-emerald-500 opacity-20 blur-[100px]" />
+      <div className="absolute bottom-0 -right-4 size-96 bg-teal-500 opacity-20 blur-[100px]" />
 
     <Routes>
       <Route path="/" element={authUser ? <ChatPage />: <Navigate to={"/login"}/>} />
@@ -28,7 +28,7 @@ const App = () => {
       <Route path="/signup" element={!authUser ? <SignUpPage /> : <Navigate to={"/"} />} />
     </Routes>
 
-    <Toaster/>
+    <CustomToast/>
     </div>
   );
 };
--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -17,18 +17,19 @@ const LoginPage = () => {
   }
 
   return (
-      <div className="w-full flex items-center justify-center p-4 bg-slate-900">
+      <div className="w-full flex items-center justify-center p-4 bg-zinc-900">
       <div className="relative w-full max-w-6xl md:h-[800px] h-[650px]">
         <BorderAnimatedContainer>
           <div className="w-full flex flex-col md:flex-row">
             {/*  LEFT SIDE */}
-            <div className="md:w-1/2 p-8 flex items-center justify-center md:border-r border-slate-600/30">
+            <div className="md:w-1/2 p-8 flex items-center justify-center md:border-r border-zinc-600/30">
               <div className="w-full max-w-md">
                 {/* HEADING TEXT */}
                 <div className="text-center mb-8">
-                  <MessageCircleIcon className="w-12 h-12 mx-auto text-slate-400 mb-4" />
-                  <h2 className="text-2xl font-bold text-slate-200 mb-2">Welcome Back</h2>
-                  <p className="text-slate-400">Login to access to your account</p>
+                  {/* <MessageCircleIcon className="w-12 h-12 mx-auto text-zinc-400 mb-4" /> */}
+                  <img src="/quickChat-logo.png" alt="logo" className="w-12 h-12 mx-auto mb-2" />
+                  <h2 className="text-2xl font-bold text-zinc-200 mb-2">Welcome Back</h2>
+                  <p className="text-zinc-400">Login to access to your account</p>
                 </div>
 
                 {/* FORM */}
@@ -84,15 +85,15 @@ const LoginPage = () => {
             </div>
 
             {/* RIGHT SIDE */}
-            <div className="hidden md:w-1/2 md:flex items-center justify-center p-6 bg-gradient-to-bl from-slate-800/20 to-transparent">
+            <div className="hidden md:w-1/2 md:flex items-center justify-center p-6 bg-gradient-to-bl from-zinc-800/20 to-transparent">
               <div>
                 <img
                   src="/login.png"
                   alt="People using mobile devices"
-                  className="w-full h-auto object-contain"
+                  className="w-full h-auto object-contain "
                 />
                 <div className="mt-6 text-center">
-                  <h3 className="text-xl font-medium text-cyan-400">Connect anytime, anywhere</h3>
+                  <h3 className="text-xl font-medium text-emerald-400">Connect anytime, anywhere</h3>
 
                   <div className="mt-4 flex justify-center gap-4">
                     <span className="auth-badge">Free</span>
